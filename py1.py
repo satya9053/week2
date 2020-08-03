93fe019247919aee340205672aaa042996da60a1
satya9053 <68903208+satya9053@users.noreply.github.com>
2020-08-03 21:35:59 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,38 @@
+
+s="sachin"
+d="sachi"
+a="hh"
+b=" bb"
+print(a+b)
+z,x,c,v,b,n=1,2,3,4,5,5
+print(z,x,c,v,b,n)
+a=b=c=d=1000
+print(a,b,c,d)
+
+kit=100
+kat=50
+print(kit+kat,kit-kat,kit*kat,kit/kat)
+t=10
+c=9
+
+t,c = c,t
+print("value of t:",t)
+print("value of c:",c)
+#type converstion
+v=10.97
+print(type(v))
+k=int(v)
+print(k)
+#datatypes integer,complex,boolean,float
+a=33
+print(type(a))
+
+f=4+7j
+print(type(f))
+
+a= True #boolean
+b= "True"
+print(type(a))
+#input() #scanf
+a=input("enter name")
+print(a)
